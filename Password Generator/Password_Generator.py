891e257e31d43bcbfa3370eec04f195515883cec
Jürgen Altszeimer <Jrgen Altszeimer>
2016-12-13 13:38:52 +0100

    Add configuration for minimum requirements
    New optional flags are available for defining a minimum of uppercases,
    lowercases, digits or symbols

--- Password Generator/Password_Generator.py
+++ Password Generator/Password_Generator.py
@@ -1,25 +1,37 @@
 import argparse
 import string
-import random
+from random import SystemRandom
 
 def generate_password(alphabet, length):
-    return ''.join(random.choice(alphabet) for _ in range(length))
+    return ''.join(SystemRandom().choice(alphabet) for _ in range(length))
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-l", "--length", dest = "length", help = "The number of characters for the password")
-parser.add_argument("-a", "--alphabet", dest = "alphabet", help = "The alphabet of the valid characters of the password")
+parser.add_argument("-l", "--length", type=int, help = "Length of the password", default = 15)
+parser.add_argument("-a", "--alphabet", help = "The alphabet of the valid characters", default = string.ascii_letters + string.digits + string.punctuation)
+requirements = parser.add_argument_group("Requirements")
+requirements.add_argument("--upper", type=int, help = "Number of minimum uppercases", default = 0)
+requirements.add_argument("--lower", type=int, help = "Number of minimum lowercases", default = 0)
+requirements.add_argument("--digits", type=int, help = "Number of minimum digits", default = 0)
+requirements.add_argument("--symbols", type=int, help = "Number of minimum symbols", default = 0)
+
 
 args = parser.parse_args()
 
-if(args.alphabet):
-    alphabet = args.alphabet
-else:
-    alphabet = string.ascii_letters + string.digits + string.punctuation
+allowed_uppercases = ''.join(c for c in args.alphabet if c in string.ascii_lowercase)
+allowed_lowercases = ''.join(c for c in args.alphabet if c in string.ascii_uppercase)
+allowed_digits = ''.join(c for c in args.alphabet if c in string.digits)
+allowed_symbols = ''.join(c for c in args.alphabet if c in string.punctuation)
 
-if(args.length):
-    length = int(args.length)
-else:
-    length = 15
 
-print(generate_password(alphabet, length))
+requiredLength = args.lower + args.upper + args.digits + args.symbols
+if args.length < requiredLength:
+    args.length = requiredLength
+
+password = generate_password(allowed_uppercases, args.upper)
+password += generate_password(allowed_lowercases, args.lower)
+password += generate_password(allowed_digits, args.digits)
+password += generate_password(allowed_symbols, args.symbols)
+password += generate_password(args.alphabet, args.length - requiredLength)
+
+print(''.join(SystemRandom().sample(password, len(password))))
